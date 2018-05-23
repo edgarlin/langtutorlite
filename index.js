8b2b3cec60eaa4a9e62fa8199ecf7c0fe3f2ecc1
edgar lin <edgarlin@users.noreply.github.com>
2018-05-24 00:56:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-
 const app = express();
 
 var post_handler = function(req,res) {
 	res.render('pages/index_post');
 };
 
-
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 

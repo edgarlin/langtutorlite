4f0e0189b5de73332c3df3eef82e5f281c7e75ad
edgar <edgar>
2018-05-27 18:39:57 +0800

    update

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const express = require('express');
-const servlet_app = require('servlets/servlet1');
-const filter_app = require('filters/filter1');
-
+const path = require('path');
+//const servlet_app = require('servlets/servlet1');
+//const filter_app = require('filters/filter1');
 //const fs = require('fs');
 //const http = require('http');
 
-
-
-
-
-
 var rootApp = express();
-var servletApp = servlet_app();
+//var servletApp = servlet_app();
 
-const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-rootApp.use(['/adm*n', '/manager'], servletApp);
-
-rootApp.use(filter_app({ option1: '1', option2: '2' }));
+//rootApp.use(['/adm*n', '/manager'], servletApp);
+//rootApp.use(filter_app({ option1: '1', option2: '2' }));
 
 rootApp.use(express.static(path.join(__dirname, 'public')));
 rootApp.set('views', path.join(__dirname, 'views'));
 rootApp.set('view engine', 'ejs');
 rootApp.get('/', (req, res) => res.render('pages/index'));
-
 rootApp.get('/port', (req, res) => res.write(PORT));
-
 rootApp.listen(PORT, () => console.log(`Listening on ${ PORT }`));
